1416457af44f50e1a35475ad499a1bb4e7c29966
Jasper den Ouden <o.jasper@gmail.com>
2015-02-15 23:17:33 +0100

    Cast wider net, not caring about case..

--- enter_pw.js
+++ enter_pw.js
@@ -1,14 +1,18 @@
 
+// List of elements to find them in.
 var list = document.getElementsByTagName("input");
 // Find the forms.
 var user_form = null;
-var passwd_form = document.activeElement; //hrmm.. there goes an option..
+var passwd_form = document.activeElement;
 
+// TODO user regexes? Whats the priority?
+// Also for (ad)block is there a way to interact with user to find the right things?
 for(var i=0 ; i<list.length ; i++){
-    if((list[i].name == "user" || list[i].name == "login" ) &&
+    var name = list[i].name.toLowerCase();
+    if(( name == "user" || name == "user_name" || name == "username" || name == "login" ) &&
        list[i].type == "text"){ user_form = list[i]; }
     if({%seekPasswdForm}) {  // Finding can be turned off just in case.
-        if(list[i].type == "password") // TODO this might not be fool-proof.
+        if(list[i].type.toLowerCase() == "password") // TODO this might not be fool-proof.
         { passwd_form = list[i]; }
     }
 }
