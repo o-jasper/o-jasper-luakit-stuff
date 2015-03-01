eca863a455caa8997b52b54d0de238d55ff1616c
Jasper den Ouden <o.jasper@gmail.com>
2015-03-01 18:55:54 +0100

    lotta messing undoing the incorporate stuff, and  after: before: and a time(difference) -from-userstring from that.

--- listview/sql_help.lua
+++ listview/sql_help.lua
@@ -9,6 +9,30 @@ require "listview.common"
 
 local string_split = lousy.util.string.split
 
+local function time_interpret(str, from_t)
+   local i, j = string.find(str, "[%-]?[%d]+[.]?[%d]*")
+   if j == 0 then
+      i, j = string.find(str, "[%-]?[%d]*[.]?[%d]+")
+   end
+   if not i or i > 2 then return nil end
+   if i == 2 then
+      if string.sub(str, 1, 1) == "a" then from_t = 0 else return nil end
+   end
+   print(string.sub(str, i, j), from_t, i, j)
+
+   -- TODO next to time-differences could also do 'day before'
+   local num = tonumber(string.sub(str, i or 1, j ~= 0 and j or #str))
+   if j == #str then return 1000*((from_t or cur_time_ms()) + 1000*num) end
+
+   local min = 60000
+   local h = 60*min
+   local d = 24*h
+   local f = ({ms=1, s=1000, min=min, h=h, d=d, D=d,
+               M=31*d, Y=365.25*d})[string.sub(str, j + 1)]
+   
+   return f and 1000*((from_t or cur_time_ms()) + num*f)
+end
+
 local function portions(str)
    local list = {}
    for i, el in pairs(string_split(str, "\"")) do
@@ -54,159 +78,168 @@ local sql_help_meta = metatable_for({
 determine = {},
 direct = {
    extcmd = function(self) return function(str, ...)
-         if self.first and self.first == "boolean" then self.first = "WHERE" end
-         table.insert(self.cmd, string.format((self.first or "") .. str, ...))
+         if self.c then
+            str = self.c .. " " .. str
+            self.c = false
+         end
          self.first = false
+         table.insert(self.cmd, string.format(str, ...))
    end end,
 
-   incorporate = function(self) return function(subhelp)
-         for _, c in pairs(subhelp.cmd)   do table.insert(self.cmd,   c) end
-         for _, i in pairs(subhelp.input) do table.insert(self.input, i) end
+   inp = function(self) return function(what)
+         if type(what) == "table" then
+            for k,v in pairs(what) do print(k, v) end
+         elseif type(what) == "number" then
+            what = tostring(what)
+         end
+         assert(type(what) == "string",
+                string.format("E(BUG): Not a string %s", what))
+         table.insert(self.input, what)
+   end end,
+   
+   addstr = function(self) return function(str, ...)
+         self.cmd[#self.cmd] = self.cmd[#self.cmd] .. string.format(str, ...)
+   end end,
+
+   comb = function(self) return function(how, down)
+         self.c = self.first or how or self.how
    end end,
 
    equal_one_or_list = function (self) return function(which, input)
-         if type(input) == "string" then
-            table.insert(self.cmd, self.extcmd("%s == ?", which))
-            table.insert(self.input, input)
-         else
-            assert(type(input) == "table")
-            
-            local str = {string.format("%s IN (?", which)}
-            for i, f in pairs(input) do
-               table.insert(self.input, f)
-               if i ~= 1 then table.insert(str, "?") end
+         if type(input) == "table" then
+            if #input > 1 then
+               local str = {string.format("%s IN (?", which)}
+               for i, f in pairs(input) do
+                  self.inp(f)
+                  if i ~= 1 then table.insert(str, "?") end
+               end
+               self.extcmd(table.concat(str, ", ") .. ")")
+               return
             end
-            self.extcmd(table.concat(str, ", ") .. ")")
+            input = input[1]
          end
+         assert(type(input) == "string")
+         self.extcmd("%s == ?", which)
+         self.inp(input)
    end end,
 
-   -- TODO just higher-then and lower-then instead of this.
-   sql_range  = function (self) return function(which, from, to)  -- TODO
-         if type(from) == "table" then
-            assert(not to)
-            to = from[2]
-            from = from[1]
-         end
-         if from == to then
-            self.extcmd("%s == ?", which)
-            table.insert(self.input, from)
-         else
-            if from then
-               self.extcmd("%s >= ?", which)
-               table.insert(self.input, from)
-            end
-            if to then
-               self.extcmd("%s <= ?", which)
-               table.insert(self.input, to)
-            end
-         end
+   lt  = function (self) return function(which, value)  -- TODO
+         self.extcmd([[%s < ?]], which)
+         self.inp(value)
+   end end,
+   gt  = function (self) return function(which, value)  -- TODO
+         self.extcmd([[%s > ?]], which)
+         self.inp(value)
    end end,
 
-   -- Note assumes `taggings` exists.
-   tags = function (self) return function(tags, w)
-         if #tags == 0 then return end
-         local h = sql_compose(self.first or self.how, true, string.format([[%sEXISTS (
-SELECT * FROM taggings]], w or ""))
-         h.extcmd([[to_id == m.id)]])
-         h.equal_one_or_list("tag", tags)
-         
-         self.incorporate(h)
+   after = function(self) return function(time)
+         self.gt(self.config.time or "id", time)
    end end,
-   not_tags = function(self) return function(tags)
-         self.tags(tags, "NOT ")
+   before = function(self) return function(time)
+         self.gt(self.config.time or "id", time)
    end end,
 
    like = function(self) return function(value, what, n)
          self.extcmd([[%s LIKE ?]], n and what .." NOT" or what)
-         table.insert(self.input, value)
+         self.inp(value)
    end end,
    not_like = function(self) return function(value, what)
          self.like(value, what, true)
    end end,
 
    text_like = function(self) return function(search, n)
-         local h = sql_compose("OR", (self.first or self.how))
-         for _, what in pairs({"title", "uri", "desc"}) do
-            h.like(search, n and what .. " NOT" or what)
+         if self.first then self.first = self.first .. "(" end
+         for i, what in pairs(self.config.textlike or {"title", "uri", "desc"}) do
+            self.comb((i == 1 and self.c .. "(") or "OR")
+            self.like(search, n and what .. " NOT" or what)
          end
-         table.insert(h.cmd, "(")
-         self.incorporate(h)
-         self.first = false
+         self.addstr(")")
    end end,
 
    text_sw = function(self) return function(search, n)
-         return self.text_like('%' .. search .. '%', n)
+         if #search > 0 then
+            self.text_like('%' .. search .. '%', n)
+         end
    end end,
    like_sw = function(self) return function(search, what, n)
-         return self.like('%' .. search .. '%', what, n)
+         if search > 0 then
+            self.like('%' .. search .. '%', what, n)
+         end
+   end end,
+
+   tags = function (self) return function(tags, w)
+         if #tags == 0 then return end
+         self.extcmd([[%sEXISTS (
+SELECT * FROM %s
+WHERE to_id == m.id]], w or "", self.config.taggings or "taggings")
+         self.comb("AND")
+         self.equal_one_or_list("tag", tags)
+         self.addstr(")")
+   end end,
+   not_tags = function(self) return function(tags)
+         self.tags(tags, "NOT ")
    end end,
 
    search = function(self) return function(str)
          local matchable = {"like:", "-like:", "tags:", "-tags", "-", "not:", "\\-", "or:",
                             "uri:", "desc:", "title:",
-                            "urilike:", "desclike:", "titlelike:"}
+                            "urilike:", "desclike:", "titlelike:",
+                            "before:", "after:"}
          local tagged_list = tagged(portions(str), matchable)
 
-         local n, tags, not_tags = false, {}, {}
-         local h = sql_compose("AND", self.first)
+         local n, tags, not_tags, before_t, after_t = false, {}, {}, nil, nil
          for i, el in pairs(tagged_list) do
             local m, v = el.m, el.v
-            print(m, v)
+            self.comb()
             local reset = true
             if m == "-like:" or m == "-lk:" or m == "like:" or m == "lk:" then
-               h.text_like(v, try, string.sub(m, 1, 2) == "-")
+               self.text_like(v, try, string.sub(m, 1, 2) == "-")
             elseif m == "tags:" then
                for _, t in pairs(string_split(v, "[,;]")) do table.insert(tags, t) end
             elseif m == "-tags:" then
                for _, t in pairs(string_split(v, "[,;]")) do table.insert(not_tags, t) end
             elseif m == "not: " or m == "-" then
                n = n or (m == "not:")
-               h.text_sw(v, true)
+               self.text_sw(v, true)
             elseif m == "\\-" then
-               h.text_sw("-" .. v, n)
+               self.text_sw("-" .. v, n)
             elseif m == "or:" then  -- NOTE `or:` takes precidence here!!
-               h.how = "OR"
+               self.how = "OR"
                if v then 
-                  h.text_sw(w, n) 
+                  self.text_sw(w, n)
                else -- Wait a sec.
                   reset = true
                end
             elseif m == "uri:" or m == "desc:" or m == "title:" then
-               h.like_sw(v, string.sub(m, 1, #m - 1), n)
+               self.like_sw(v, string.sub(m, 1, #m - 1), n)
             elseif m == "urilike:" or m == "desclike:" or m == "titlelike:" then
-               h.like(v, string.sub(m, 1, #m-5), n)
-            --elseif m == "after:" then
-            --elseif m == "before:" then
+               self.like(v, string.sub(m, 1, #m-5), n)
+            elseif m == "after:" and time_interpret(v) then
+               after_t = math.max(after_t or 0, time_interpret(v))
+            elseif m == "before:" and time_interpret(v) then
+               if before_t then
+                  before_t = math.min(before_t, time_interpret(v))
+               else
+                  before_t = time_interpret(v)
+               end
             else
-               h.text_sw(v, n)
+               self.text_sw(v, n)
             end
-            if reset then h.how = "AND" end
+            if reset then self.how = "AND" end
          end
-         h.how = "AND"
-         h.tags(tags)
-         h.not_tags(not_tags)
-         
-         -- TODO for .. tags
-         self.incorporate(h)
-         self.first = false
-   end end,
-
-   -- NOTE/TODO not getting use, might remove.
-   ui_data_selection = function(self) return function(config)
-         if config.kind then self.equal_one_or_list("kind", config.kind) end
-         if config.from then self.equal_one_or_list("origin", config.origin) end
-         if config.must_tags then self.require_tags(config.must_tags) end
-         if config.must_not_tags then self.require_tags(config.must_tags, " NOT ") end
-         if config.search then self.search(config.search) end
-         if config.claimtime then self.range("claimtime", config.claimtime) end
-         if config.time then self.range("id", config.time) end
-         if config.re_assess_time then self.range("claimtime", config.re_assess_time) end
+         self.how = "AND"
+         self.comb()
+         self.tags(tags)
+         self.comb()
+         self.not_tags(not_tags)
+         if before_t then self.comb() self.before(before_t) end
+         if after_t then self.comb() self.after(after_t) end
    end end,
 
    order_by = function(self) return function(what, way)
          if type(what) == "table" then what = table.concat(what, ", ") end
-         table.insert(self.cmd, 
-                      string.format("ORDER BY %s %s", what, way or "DESC"))
+         self.c = ""
+         self.extcmd("ORDER BY %s %s", what, way or "DESC")
    end end,
       
    sql_pattern = function(self) return function()
@@ -234,23 +267,14 @@ SELECT * FROM taggings]], w or ""))
 
 }})
 
-function sql_compose(how, first, initial)  -- Less involved a bit.
-   if first == nil then first = true end
-   local helper = {cmd = initial and {initial} or {}, first=first,
-                   input = {},
-                   how = how or "AND" }
-   setmetatable(helper, sql_help_meta.table)
-   return helper
-end
-
-function new_sql_help(how, initial, db, fun)
+function new_sql_help(how, initial, db, fun, config)
    if type(initial) == "table" then -- Is a list of stuff we want.
       initial = string.format("SELECT %s FROM msgs m", table.concat(initial, ", "))
    end
-   local helper = {db = db, fun = fun, first=true,
+   local helper = {db = db, fun = fun, first=first or  "WHERE", c = false,
                    cmd = {initial or [[SELECT * FROM msgs m]]},
                    input = {},
-                   how = how or "AND" }
+                   how = how or "AND", config = config or {}}
    setmetatable(helper, sql_help_meta.table)
    return helper
 end
