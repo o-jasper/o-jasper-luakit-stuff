0900cf8f6b4e74308372c87c62f4c230ffd8fe8d
Jasper den Ouden <o.jasper@gmail.com>
2015-04-26 22:38:12 +0200

    ability to take luakit://history and the command (not really needed)

--- listview/history/chrome.lua
+++ listview/history/chrome.lua
@@ -5,8 +5,6 @@
 --  by the Free Software Foundation, either version 3 of the License, or
 --  (at your option) any later version.
 
--- Chrome page of all logs. User may do subselection.
-
 require "listview.chromable"
 require "listview.history"
 
@@ -14,33 +12,36 @@ local table_copy = require("o_jasper_common.other").table_copy
 
 local paged_chrome = require("paged_chrome")
 
-local config = globals.hist_n_bookmarks or {}
+local config = (globals.listview or {}).history or {}
 config.page = config.page or {}
 
 local function chrome_describe(default_name, log)
    assert(log)
    local page = listview_chrome(log, "search", "listview/history")
-   page.set_cnt = config.page.cnt or 20
-   page.set_step = config.page.step or page.step_cnt
+   page.limit_cnt = config.page.cnt or 20
+   page.limit_step = config.page.step or page.step_cnt
    return { default_name = default_name,
             search = paged_chrome.templated_page(page),
    }
 end
 
--- Turned on defaultly.
-if true or config.take_history then
-   paged_chrome.paged_chrome("listviewHistory", chrome_describe("search", history))
+local history_paged = chrome_describe("search", history)
+paged_chrome.paged_chrome("listviewHistory", history_paged)
+
+if config.take_history_chrome then  -- Take over the 'plain name'. (default:no)
+   paged_chrome.paged_chrome("history", history_paged)
 end
 
 local cmd = lousy.bind.cmd
 
-add_cmds({
-            cmd("listviewHistory",
-                function(w, query)
-                   history.latest_query = query
-                   local v = w:new_tab("luakit://listviewHistory/search")
-                   -- if query then  -- This would be without the nasty "global value" thing.
-                   --  v:eval_js(string.format("ge('search').value = %q; search();", query))
-                   -- end
-                end),
-         })
+local function on_command(w, query)
+   history.latest_query = query
+   local v = w:new_tab("luakit://listviewHistory/search")
+   -- if query then  -- This would be without the nasty "global value" thing.
+   --  v:eval_js(string.format("ge('search').value = %q; search();", query))
+   -- end
+end
+
+add_cmds({ cmd("listviewHistory", on_command) })
+
+if config.take_history_cmd then add_cmds({ cmd("history", on_command) }) end
