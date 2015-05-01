370851f66ca148106825f914a5c2e7688f878f5f
Jasper den Ouden <o.jasper@gmail.com>
2015-05-01 18:22:50 +0200

    Criminally missing files.

---
+++ listview/assets/search_initial.js
@@ -0,0 +1,11 @@
+set_by_search(true);
+set_sql_locked(false);
+set_sql_shown({%sqlShown});
+set_addsearch_shown(false);
+set_as_msg(true);
+set_continuous(true);
+set_actions_panel(false);
+set_safe_mode(true);
+ge('search_input').value = "{%latestQuery}"
+touch_addsearch_name();
+search();
