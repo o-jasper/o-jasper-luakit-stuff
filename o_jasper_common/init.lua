031b17c05d98807fe97826b8bf628d74b084d0c7
Jasper den Ouden <o.jasper@gmail.com>
2015-05-06 17:29:08 +0200

    Split off `full_gsub`

--- o_jasper_common/init.lua
+++ o_jasper_common/init.lua
@@ -6,6 +6,8 @@ for k,v in pairs(require "o_jasper_common.meta")   do Public[k] = v end
 for k,v in pairs(require("o_jasper_common.other")) do Public[k] = v end
 Public.load_asset = require "o_jasper_common.load_asset"
 
+Public.full_gsub = require "o_jasper_common.full_gsub"
+
 Public.tableText = require("o_jasper_common.text").tableText
 
 return Public
--- o_jasper_common/other.lua
+++ o_jasper_common/other.lua
@@ -51,14 +51,4 @@ function Public.values_now_set(tab)
    return ret
 end
 
--- TODO.. the pattern is a bit arbitrary.
-function Public.full_gsub(str, subst)  -- Perhaps something for lousy.util.string
-   local n, k = 1, 0
-   while n > 0 and k < 256 do
-      str, n = string.gsub(str, "{%%([_./%w]+)}", subst)
-      k = k + 1
-   end
-   return str
-end
-
 return Public
