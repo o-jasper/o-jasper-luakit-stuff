8a120e297227ea0d8887bf18c3c3e95b67e5b4f9
Jasper den Ouden <o.jasper@gmail.com>
2015-05-24 19:58:56 +0200

    Separate off info

--- urltamer/handler.lua
+++ urltamer/handler.lua
@@ -10,78 +10,15 @@ local domain_status = require "urltamer.domain_status"
 local ensure = require "o_jasper_common.ensure"
 local domain_of_uri = require("o_jasper_common.fromtext.uri").domain_of_uri
 
+local cur_time = require "o_jasper_common.cur_time"
+
 local config = globals.urltamer or {}
 
-local cur_time = require "o_jasper_common.cur_time"
+local new_info  = require("urltamer.info").new_info
 
 config.late_dt = config.late_dt or 4000
 config.log_all = (config.log_all == nil) or config.log_all
 config.logger = require "urltamer.print_logger"
-
--- Values directly returned, for instance member functions.
-local info_metaindex_direct = {
-   own_domain = function(self)
-      return self.from_domain == "no_vuri" or self.from_domain == self.domain
-   end,
-
-   uri_match = function(self, match)
-      for i, el in ensure.pairs(match) do
-         if string.match(self.uri, el) then 
-            return i
-         end
-      end
-   end,
-}
-
--- Values that are determined and memoized. (though memoizing is not necessarily better)
-local info_metaindex_determine = {
-   tags = function(self, _) return "" end,
-   domain = function(self, _) return domain_of_uri(self.uri or "no_uri") end,
-   
-   vuri = function(self, _) return self.v.uri end,
-   from_domain = function(self, _) return domain_of_uri(self.vuri or "no_vuri") end,
-
-   current_status = function(self, _)
-      local got = domain_status._status[self.from_domain]
-      if not got then
-         got = {}
-         assert(self.from_domain, string.format("aint got (%s)", self.from_domain))
-         domain_status._status[self.from_domain] = got
-      end
-      return got
-   end,
-   status = function(self, _) return self.current_status.status end,
-
-   -- TODO destructive.. probably dont want.
-   from_time = function(self, _) 
-      return (self.current_status.times or {})["userevent-uri"] or 0
-   end, 
-   dt = function(self, _) return self.time - self.from_time end,
-}
-
-local info_metatable = {__index=function(self, key)
-   local got = rawget(self, key) or info_metaindex_direct[key]
-  -- Return value, because set, or because specified by metatable.c
-   if got or type(got) == "boolean" then
-      return got
-   else
-      local determiner = info_metaindex_determine[key]
-      if determiner then  -- To be determined by functions.
-         local val = determiner(self, key)
-         rawset(self,key, val)
-         return val
-      end
-      return nil
-   end
-end,
-   -- TODO setting indexes?
-}
-
-function new_info(v, uri)
-   local info = {v=v, uri=uri, time=cur_time.ms()}
-   setmetatable(info, info_metatable)
-   return info
-end
 
 handler = {}
 
