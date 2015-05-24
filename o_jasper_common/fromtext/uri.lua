f329da4a6a2af63b1655e38cdede09e7cabc5009
Jasper den Ouden <o.jasper@gmail.com>
2015-05-24 19:53:29 +0200

    Added the move before, not the changes

--- o_jasper_common/fromtext/uri.lua
+++ o_jasper_common/fromtext/uri.lua
@@ -1,15 +1,17 @@
 -- This should be in lib/lousy/uri.lua ?
-function domain_of_uri(uri)
-   if string.match(uri, "^file://.+") then
-      return "file://"
-   else
-      local _, s = string.find(uri, "//")
-      if s then
-         local e, _ = string.find(uri, "/", s+1)
-         e = e or #uri + 1
-         return string.sub(uri, s + 1, e - 1)
+return {
+   domain_of_uri = function(uri)
+      if string.match(uri, "^file://.+") then
+         return "file://"
       else
-         return "unknown"
+         local _, s = string.find(uri, "//")
+         if s then
+            local e, _ = string.find(uri, "/", s+1)
+            e = e or #uri + 1
+            return string.sub(uri, s + 1, e - 1)
+         else
+            return "unknown"
+         end
       end
    end
-end
+}
