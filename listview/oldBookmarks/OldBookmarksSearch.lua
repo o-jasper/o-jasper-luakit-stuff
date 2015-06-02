5255caf04fd1ccfd966fbc19809645161f91fd5a
Jasper den Ouden <o.jasper@gmail.com>
2015-06-02 18:33:03 +0200

    minor mistakes oldbookmarks

--- listview/oldBookmarks/OldBookmarksSearch.lua
+++ listview/oldBookmarks/OldBookmarksSearch.lua
@@ -1,7 +1,10 @@
+local c = require "o_jasper_common"
+local Search = require "listview.Search"
+
 local this = c.copy_meta(require "listview.Search")
 
 function this:repl_list(args)
-   local ret = listview.Search.repl_list(self, args)
+   local ret = Search.repl_list(self, args)
    ret.above_title = [[<br><span class="warn">This is just thrown together quickly,
 the other bookmarks is the "serious" one.</span>]]
    return ret
