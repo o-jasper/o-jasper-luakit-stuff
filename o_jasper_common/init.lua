33cfa065faee281cb4aa23561624ba7da085d8d6
Jasper den Ouden <o.jasper@gmail.com>
2015-06-02 22:42:32 +0200

    Add bit out-of place `init.lua`

--- o_jasper_common/init.lua
+++ o_jasper_common/init.lua
@@ -1,13 +1,16 @@
 --error("Doesnt exist at this point")
-local Public = {}
+local this = {}
 
-Public.cur_time = require "o_jasper_common.cur_time"
-for k,v in pairs(require "o_jasper_common.meta")   do Public[k] = v end
-for k,v in pairs(require "o_jasper_common.other") do Public[k] = v end
-for k,v in pairs(require "o_jasper_common.load_asset") do Public[k] = v end
+this.cur_time = require "o_jasper_common.cur_time"
+this.ensure = require "o_jasper_common.ensure"
+this.from_text = require "o_jasper_common.from_text"
 
-Public.full_gsub = require "o_jasper_common.full_gsub"
+for k,v in pairs(require "o_jasper_common.meta")   do this[k] = v end
+for k,v in pairs(require "o_jasper_common.other") do this[k] = v end
+for k,v in pairs(require "o_jasper_common.load_asset") do this[k] = v end
 
-for k,v in pairs(require "o_jasper_common.text") do Public[k] = v end
+this.full_gsub = require "o_jasper_common.full_gsub"
 
-return Public
+for k,v in pairs(require "o_jasper_common.text") do this[k] = v end
+
+return this
