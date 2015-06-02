63841fb994b8b1d55ef04fec693920fd92147dd9
Jasper den Ouden <o.jasper@gmail.com>
2015-06-02 22:48:31 +0200

    mistakes..

--- o_jasper_common/fromtext/init.lua
+++ o_jasper_common/fromtext/init.lua
@@ -1,6 +1,6 @@
 local function index(self, key)  -- Get until we got it.
-   for name in pairs({"searchlike", "time", "uri"}) do
-      for k,v in pairs(require("o_jasper_common." .. name)) do
+   for name in pairs({searchlike=true, time=true, uri=true, unit=true}) do
+      for k,v in pairs(require("o_jasper_common.fromtext." .. name)) do
          self[k] = v
       end
       if self[k] then return self[k] end
---
+++ o_jasper_common/fromtext/unit.lua
@@ -0,0 +1,9 @@
+return {
+   w_magnitude_interpret = function(text)
+      if string.match(text, "^[%d]+[ ]*[numkMGT]*$") then
+         local name = string.match(text, "*[numkMGT]*$")
+         local factor = ({p=1e-12, n=1e-9, u=1e-6, m=1e-3, 
+                          k=1e3, M=1e6, G=1e9, T=1e12})[name] or 1
+         return tonumber(string.match(text, "^[%d]+"))*factor
+   end,
+}
