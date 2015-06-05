d2729c4d0759a6dd99ac06c7411b40e35c509ccf
Jasper den Ouden <o.jasper@gmail.com>
2015-06-05 03:38:45 +0200

    base64 thanks to Alex Kloss, lua-users.org/wiki/BaseSixtyFour, and whoever in that small competition.. And images, based on it

--- dirChrome/infofun/img.lua
+++ dirChrome/infofun/img.lua
@@ -5,8 +5,8 @@ local c = require "o_jasper_common"
 local this = {}
 
 function this.maybe_new(path, file, dir)
-   for _, pat in pairs({"[.]jpg", "[.]jpeg",
-                        "[.]gif", "[.]bmp", "[.].png", "[.]svg" }) do
+   for _, pat in pairs({"[.]jpg$", "[.]jpeg$",
+                        "[.]gif$", "[.]bmp$", "[.]png$", "[.]svg$" }) do
       if string.match(string.lower(file), pat) then
          return setmetatable({ path=path, file=file }, this)
       end
@@ -14,12 +14,17 @@ function this.maybe_new(path, file, dir)
 end
 
 function this:priority()
-   return 1 -- For now.. should be context-dependent..
+   return 1 -- TODO For now.. should be context-dependent.. and need see-what-you-select.
 end
 
 function this:html()  -- TODO not allowed to touch local shit..
    local fp = self.path .. "/" .. self.file
-   return string.format([[<a href="file:/%s"><img src="file://%s"></a>]], fp, fp)
+   local got = c.base64.enc_file(fp)
+   local format = string.match(self.file, "[.][%a]+")
+   if got and format then
+      return string.format([[%s<img src="data:image/%s;base64,%s", alt="%s">]],
+         fp, string.sub(format, 2), got, fp)
+   end
 end
 
 return c.metatable_of(this)
--- o_jasper_common/init.lua
+++ o_jasper_common/init.lua
@@ -1,15 +1,14 @@
 --error("Doesnt exist at this point")
-local this = {}
+local function index(d, key)
+   assert(type(key) == "string")
+   return require("o_jasper_common." .. key)
+end
 
-this.cur_time = require "o_jasper_common.cur_time"
-this.ensure = require "o_jasper_common.ensure"
-this.fromtext = require "o_jasper_common.fromtext"
+local this = setmetatable({}, {__index = index})
 
 for k,v in pairs(require "o_jasper_common.meta")   do this[k] = v end
 for k,v in pairs(require "o_jasper_common.other") do this[k] = v end
 for k,v in pairs(require "o_jasper_common.load_asset") do this[k] = v end
-
-this.full_gsub = require "o_jasper_common.full_gsub"
 
 for k,v in pairs(require "o_jasper_common.text") do this[k] = v end
 
