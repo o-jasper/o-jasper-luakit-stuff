96b40587d8e5c22015b7992139a793a9b001ec7e
Jasper den Ouden <o.jasper@gmail.com>
2015-06-05 02:40:45 +0200

    Same approach to UriRequest, also `this` instead of repeating the name

--- urltamer/sql_logger/UriRequests.lua
+++ urltamer/sql_logger/UriRequests.lua
@@ -5,40 +5,29 @@ config.addsearch = config.addsearch or { default = "" }
 local c = require "o_jasper_common"
 local UriRequestsEntry = require "urltamer.sql_logger.UriRequestsEntry"
 
-local UriRequests = c.copy_meta(require("sql_help").SqlHelp)
-UriRequests.values = UriRequestsEntry.values
+local this = c.copy_meta(require("sql_help").SqlHelp)
+this.values = UriRequestsEntry.values
 
 -- Scratch some search matchabled that arent allowed.
-UriRequests.searchinfo.matchable = {
+this.searchinfo.matchable = {
    "like:", "-like:", "-", "not:", "\\-", "or:",
-   "uri:", "urilike:",
    "before:", "after:", "limit:",
-   -- TODO these currently do nothing/bug-out, add to `searchinfo.match_funs`
-   "domain:", "from_domain:", "domainlike:", "from_domainlike:",
-   "result:", "resultlike"
 }
 
-local function dom_match_fun(sub_n, wrap) return function (self, state, m, v)
-   self:like(string.sub(m, 1, #m - sub_n), wrap .. v .. wrap, state.n)
-end end
+for _, el in pairs{"uri", "domain", "from_domain", "result"} do
+   for _, kind in pairs{"=", "like:", ":"} do
+      this.searchinfo.match_funs[el .. kind] = this.searchinfo.match_funs["uri" .. kind]
+      table.insert(this.searchinfo.matchable, el .. kind)
+   end
+end
 
-local dom_search = dom_match_fun(1, "%")
-UriRequests.searchinfo.match_funs["domain:"] = dom_search
-UriRequests.searchinfo.match_funs["from_domain:"] = dom_search
-UriRequests.searchinfo.match_funs["result:"] = dom_search
-
-local dom_like = dom_match_fun(5, "")
-UriRequests.searchinfo.match_funs["domainlike:"] = dom_like
-UriRequests.searchinfo.match_funs["from_domainlike:"] = dom_like
-UriRequests.searchinfo.match_funs["resultlike:"] = dom_like
-
-UriRequests.entry_meta = UriRequestsEntry
+this.entry_meta = UriRequestsEntry
 
 local cur_time = require "o_jasper_common.cur_time"
 
-UriRequests.last_id = 1000*cur_time.ms()
+this.last_id = 1000*cur_time.ms()
 
-function UriRequests.insert(self, info, result)
+function this:insert(info, result)
    info.id = 1000*cur_time.ms()
    while info.id <= self.last_id do
       info.id = info.id + 1
@@ -50,13 +39,13 @@ function UriRequests.insert(self, info, result)
    self:enter(info)
 end
 
-function UriRequests.config(self) return config end
+function this:config() return config end
 
 local entry_html = require "listview.entry_html"
 
 local fancy_uri = require("o_jasper_common.html.uri").fancy_uri
 
-function UriRequests.initial_state(self)
+function this:initial_state()
    local mod_html_calc = {
       resultHTML = function(entry, _)
          return ({["true"]="{%urlallowed}", ["false"]="{%urlblocked}"})[entry.result] or
@@ -78,4 +67,4 @@ function UriRequests.initial_state(self)
    return { html_calc=html_calc }
 end
 
-return c.metatable_of(UriRequests)
+return c.metatable_of(this)
