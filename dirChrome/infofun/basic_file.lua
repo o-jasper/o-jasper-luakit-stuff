395701721e775c719aeae71080e704b922257064
Jasper den Ouden <o.jasper@gmail.com>
2015-06-07 01:06:33 +0200

    better file info

--- dirChrome/infofun/basic_file.lua
+++ dirChrome/infofun/basic_file.lua
@@ -1,12 +1,7 @@
 -- Shows readme functions and/or 
 
-local config_from = (globals.dirChrome or {}).infofun or {}
--- Better not use `config.html`, add the asset.
-local config = config_from.file or config_from.basic_file or {}
-
 local c = require "o_jasper_common"
 local lfs = require "lfs"
-local entry_html = require "listview.entry_html" -- TODO there are simularities.. maybe
 
 local this = {}
 
@@ -18,31 +13,13 @@ function this:priority()
    return 0
 end
 
-function this:html(asset_fun)
+function this:html()  -- TODO not allowed to touch local shit..
    local fp = self.path .. "/" .. self.file
-
-   local funs = {
-      slashifdir = function() return mode == "directory" and "/" or " " end,
-      
-      modification_date = function(attrs) return os.date("%c", attrs.modification) end,
-      change_date       = function(attrs) return os.date("%c", attrs.change) end,
-      access_date       = function(attrs) return os.date("%c", attrs.access) end,
-
-      default = function(_, _, key)
-         return string.match(key, "^[/_.%w]+$") and asset_fun(key)
-      end,
-   }
-   local pattern_funs = {
-      ["^date_"] = function(attrs, _, key)
-         local inside = string.match(key, "_[%w]")
-         local got = attrs[string.sub(inside, 2)]
-         if type(got) == "number" then
-            return os.date(string.sub(string.match(key, "_[%w]$") or "_%c", 2),
-                           got)
-         end
-      end,
-   }
-   return c.apply_subst(config.html or asset_fun("parts/show_elaborate"))
+   local ret = {}
+   for k,v in pairs(lfs.attributes(fp)) do
+      table.insert(ret, string.format("<b>%s</b>:%s", k,v))
+   end
+   return table.concat(ret, ", ")
 end
 
 return c.metatable_of(this)
---
+++ dirChrome/infofun/file.lua
@@ -0,0 +1,60 @@
+-- Shows readme functions and/or 
+
+local config_from = (globals.dirChrome or {}).infofun or {}
+-- Better not use `config.html`, add the asset.
+local config = config_from.file or config_from.basic_file or {}
+
+local c = require "o_jasper_common"
+local lfs = require "lfs"
+local entry_html = require "listview.entry_html" -- TODO there are simularities.. maybe
+
+local this = {}
+
+function this.maybe_new(path, file)
+   return setmetatable({ path=path, file=file }, this)
+end
+
+function this:priority()
+   return 0
+end
+
+function this:repl(asset_fun)
+   local fp = self.path .. "/" .. self.file
+
+   local funs = {
+      slashifdir = function() return mode == "directory" and "/" or " " end,
+      
+      modification_date = function(attrs) return os.date("%c", attrs.modification) end,
+      change_date       = function(attrs) return os.date("%c", attrs.change) end,
+      access_date       = function(attrs) return os.date("%c", attrs.access) end,
+
+      default = function(_, _, key)
+         return string.match(key, "^[/_.%w]+$") and asset_fun(key)
+      end,
+   }
+   local pattern_funs = {
+      ["^date_"] = function(attrs, _, key)
+         local inside = string.match(key, "_[%w]")
+         local got = attrs[string.sub(inside, 2)]
+         if type(got) == "number" then
+            return os.date(string.sub(string.match(key, "_[%w]$") or "_%c", 2),
+                           got)
+         end
+      end,
+   }
+   local attrs = lfs.attributes(fp)
+   attrs.file = self.file
+   attrs.path = self.path
+   return c.repl(attrs, nil, attrs, funs, pattern_funs)
+end
+
+self.asset_file = "parts/show_elaborate.html"
+function this:repl_pattern(asset_fun)
+   return config.html or asset_fun(self.asset_file)
+end
+
+function this:html(asset_fun)
+   return c.apply_subst(self:repl_pattern(asset_fun), self:repl(asset_fun))
+end
+
+return c.metatable_of(this)
