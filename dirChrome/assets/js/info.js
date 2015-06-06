9c20196c1135adb74469d3c0749e46dac771b65f
Jasper den Ouden <o.jasper@gmail.com>
2015-06-06 20:37:12 +0200

    Selection view should show in a convenient place

--- dirChrome/assets/js/info.js
+++ dirChrome/assets/js/info.js
@@ -1,5 +1,13 @@
 function set_main_sel(to) {
-    var html = html_of_id(to);
-    if(html){ ge("infofuns_select").innerHTML = html; }
+    if(to) {
+        var html = html_of_id(to);
+        if(html){
+            ge("infofuns_select").innerHTML =
+                '<div id="infofuns_select_area" style="top:' + 
+                window.pageYOffset + 'px">' + html + "</div>";
+        }
+    } else {
+        ge("infofuns_select").innerHTML = "";
+    }
     main_sel = to;
 }
