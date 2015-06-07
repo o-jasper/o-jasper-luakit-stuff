e1ba99c605cc024bc047056297d2853c7074cb4c
Jasper den Ouden <o.jasper@gmail.com>
2015-06-07 15:37:21 +0200

    Better warning if wrong type for searching asset

--- paged_chrome/templated.lua
+++ paged_chrome/templated.lua
@@ -6,8 +6,12 @@ local Public ={}
 local c = require "o_jasper_common"
 
 function Public.asset(where, key)
-   return c.load_search_asset(where, "/assets/" .. key) or
-      string.format([[alert("ASSET NOT FOUND %s");]], key)
+   if type(key) == "string" then
+      return c.load_search_asset(where, "/assets/" .. key) or
+         string.format([[alert("ASSET NOT FOUND %s");]], key)
+   else
+      return string.format([[alert("ASSET NOT VALID %s");]], key)
+   end
 end
 
 local templated_page_metatable = {
