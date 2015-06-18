9d4632c3c9b2fd2cdaf692a67d2eb77d37ad96c4
Jasper den Ouden <o.jasper@gmail.com>
2015-06-18 15:06:20 +0200

    Limiting, thinking of separating this out to a new class

--- alt_require/ah/Table.lua
+++ alt_require/ah/Table.lua
@@ -17,7 +17,7 @@ This.new_defaults = {
    loaded = {},
 }
 
---This.allow_global = false
+--This.disallow_global = false
 This.record_require_file = true
 This.record_any = true
 This.recurse = true
@@ -72,25 +72,64 @@ function This:_ensure_tablestarts(file)
    return cnts, vals
 end
 
+function This:_newindex(file)
+   return self.disallow_global and function (_, _, key)
+      error("Trying to set %s from %q, but setting global disabled. ", key, file)
+   end
+end
+
 function This:meta(file)
    local cnts, vals = self:_ensure_tablestarts(file)
    return {
       __index = self:indexat(self.env, vals, cnts),
-      __newindex = self.allow_global and function(self, to, key)
-         error("Trying to set %s from %q, but setting global disabled. ", key, file)
-       end,
+      __newindex = self:_newindex(file),
       -- __pairs = function(_) return pairs(self.env) end,
    }
 end
 
 function This:assumed_require_fun(file)
    -- TODO use the information in values.
+   return vals[file]  -- Right.. dead-simple.
 end
 
-function This:require_fun(file)
-   if self.mode == "record" then
+function This:enforced_indexat(from, cnts)
+   assert(from)
+   return function(_, key)
+      if cnts[key] then
+         local got = from[key]
+         if type(got) == "table" then
+            return setmetatable({},
+               {__index = self:enforced_indexat(got, cnts[key])})
+         else
+            return got
+         end
+      else
+         error("aint allowed here")
+      end
+   end
+end
+
+function This:enforced_meta(file)
+   local cnts = self.cnts[file]
+   if not cnts then error("disallowed require %q", file) end
+
+   return {
+      __index = self:enforced_indexat(self.env, cnts),
+      __newindex = self:_newindex(file),
+   }
+end
+
+function This:enforced_require_fun(file)
+   return self.require_fun(self.enforced_meta)
+end
+
+function This:require_fun(file, mode)
+   local mode = self.mode or more
+   if mode == "record" then
       return SimpleBase.require_fun(self, file)
-   elseif self.mode == "assume" then
+   elseif mode == "enforce" then
+      return self:enforced_require_fun(file)
+   elseif mode == "assume" then
       return self:assumed_require_fun(file)
    else
       error("Aint got mode %s", mode)
@@ -114,6 +153,57 @@ function This:info_at_spot(file, location)
       end
    end
    return true, got, cnts, vals
+end
+
+-- TODO dubious mess..
+-- Poke at a thing as if it is being touched. 
+function This:poke(file, location, way)
+   local final_lo
+   way = way or {}
+   local cnts, vals = self:_ensure_tablestarts(file)
+   local got = self:require(file)
+   local i = 1
+   while i < #location -1 do
+      local k = location[i]
+      if type(got) == "table" then
+         got = got[k]
+         local next_cnts, next_vals = cnts[k] or {}, vals[k] == nil and {} or vals[k]
+         cnts[k] = next_cnts
+         vals[k] = next_vals
+      else
+         return "nothing to reach"  -- Failure, cant go deeper.
+      end
+      i = i + 1
+   end
+   local k = location[i]
+   vals[k] = vals[k] == nil and {} or vals[k]
+   if getmetatable(got[k]) and way.w_meta and way.w_meta == "disallow" then
+      return "disallow metatables"
+   end
+   if type(got[k]) == "table" then  -- TODO Hrmm..
+      if vals[k] and type(vals[k]) ~= "table" then
+         return "incompatible to set as table"
+      end
+      if way.table == "disallow" then 
+         return "disallow tables"
+      elseif way.table == "via pairs" then
+         vals[k] = vals[k] or {}
+         for k2,v in pairs(got[k]) do
+            vals[k][k2] = v
+         end
+      elseif way.table == "set, no meta" then
+         if getmetatable(vals[k])  then return "disallow meta" end
+         vals[k] = got[k]
+      elseif way.table == nil or way.table == "set" then
+         vals[k] = got[k]
+      else
+         error("Dont know what to do with %s for `way.table`", way.table)
+         return "fail"
+      end
+   else
+      vals[k] = got[k]
+   end
+   return "success"
 end
 
 return c.metatable_of(This)
--- alt_require/readme.md
+++ alt_require/readme.md
@@ -1,1 +1,3 @@
-Experimentation with using `load` to record, restrict things.
+Experimentation with using `load` to record, determine, and/or restrict things.
+
+Appears to work for both lua and luajit, but been used very limitedly.
