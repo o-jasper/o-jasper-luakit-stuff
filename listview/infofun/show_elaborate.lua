74d995d1878dcea4f49d535a7a7b92fe3c232d32
Jasper den Ouden <o.jasper@gmail.com>
2015-06-23 02:18:18 +0200

    have a slightly more elaborate version

--- listview/infofun/show_elaborate.lua
+++ listview/infofun/show_elaborate.lua
@@ -1,6 +1,10 @@
+local c = require "o_jasper_common"
 local This = c.copy_meta(require "listview.infofun.show_1")
 
--- Thats it.
+function This.maybe_new(_, entry) return setmetatable({ e=entry }, This) end
+
+function This:priority() return 0 end
+
 This.asset_file = "parts/show_elaborate.html"
 
 return c.metatable_of(This)
