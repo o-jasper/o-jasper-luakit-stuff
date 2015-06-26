bc345428793afd78b35dad93831b0eca1827c927
Jasper den Ouden <o.jasper@gmail.com>
2015-06-26 03:14:47 +0200

    Page info chrome and command

--- paged_chrome/examples/init.lua
+++ paged_chrome/examples/init.lua
@@ -1,3 +1,4 @@
 
 require "paged_chrome.examples.bare"
 require "paged_chrome.examples.bare_dir_chrome"
+require "paged_chrome.examples.pageinfo"
---
+++ paged_chrome/examples/pageinfo.lua
@@ -0,0 +1,66 @@
+-- Uses javascript to extract a bunch of information, and then 
+-- shows it back.
+
+local info = {
+   chrome_name = "pageInfo",
+   chrome_uri  = "luakit://{%chrome_name}",
+   title       = "{%chrome_name}",
+}
+
+local data
+
+-- Think the things that crash do so because the sequence item not 
+--  translated into lua properly?
+local acquire = {
+   referrer = "document.referrer",
+--   anchors = "document.anchors",  -- crash
+   anchors_cnt = "document.anchors.length",
+   links     = "document.links",
+   links_cnt = "document.links.length",
+
+   applets     = "document.applets",
+   applets_cnt = "document.applets.length",
+   embeds      = "document.embeds",
+   embeds_cnt  = "document.embeds.length",
+   cookie  = "document.cookie",
+   cookie_cnt  = "document.cookie.length",
+   --images  = "document.images", -- crash
+   images_cnt = "document.images.length",
+}
+
+local cmd = lousy.bind.cmd
+add_cmds({ cmd("pageinfo", function(w, query)
+                  data = {}  -- Sluurp
+                  for k,v in pairs(acquire) do data[k] = w.view:eval_js(v) end
+                  w:new_tab("luakit://" .. info.chrome_name .. "/view")
+              end) 
+})
+
+local html = require "o_jasper_common.html"
+
+local function list_text(list, name, between)
+   local ret = {}
+   for k, el in ipairs(list) do
+      table.insert(ret, tostring(type(el) == "table" and el[name] or el))
+   end
+   return table.concat(ret, between or ", ")
+end
+
+local pages = {
+   default_name = "view",
+   view = {
+      name = "view",
+      where = "paged_chrome/examples",
+      
+      repl = function(self, args)
+         for k,v in pairs(info) do data[k] = v end
+         --data.anchors_name_list = list_text(data.anchors, "name")
+         --data.img_href_list = list_text(data.images, "href", "<br>")
+
+         data.cookie_table = data.cookie --html.table(data.cookie)
+         return data
+      end,
+   }
+}
+
+require("paged_chrome").chrome(info.chrome_name, pages)
