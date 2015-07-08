bcfb5a028d1da5d797b4a582f86d624ed42c7015
Jasper den Ouden <o.jasper@gmail.com>
2015-07-08 13:35:03 +0200

    Bookmarks link to their data-uris

--- listview/bookmarks/infofun/show_1.lua
+++ listview/bookmarks/infofun/show_1.lua
@@ -14,4 +14,12 @@ function This.tab_repl:title()  -- TODO history should have This too.
    return self.e.title or self.e.uri or "(no title)"
 end
 
+function This.tab_repl.data_uri_html()
+   if package.loaded["dirChrome.chrome"] then
+      return [[<a href="luakit://dirChrome/search/{%data_uri}"><code>{%data_uri}</code></a>]]
+   else
+      return "<code>{%data_uri}</code>"
+   end
+end
+
 return c.metatable_of(This)
