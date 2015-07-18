164d190469f6437f7da93920699603b3bf1b8cf9
Jasper den Ouden <o.jasper@gmail.com>
2015-07-19 00:20:38 +0200

    Navigable chrome/page-not-founds.

--- paged_chrome/pegasus/plain.lua
+++ paged_chrome/pegasus/plain.lua
@@ -5,6 +5,35 @@ local Suggest = require "paged_chrome.Suggest"
 
 return function(register)
    local server = Pegasus:new()
+
+   local function help_if_not_found(chrome, args)
+      --html = string.format("<p>Try.. %d</p> %s %s %s <p>%s %s</p>", k, req.path,
+      -- t, t2, args.page, args.path)
+      local html = "Dont have this..<h4>args:</h4><table>"
+      for k, v in pairs(args) do
+         html = html .. string.format("<tr><td>%s =</td><td>%s</td></tr>", k,v)
+      end
+      html = html .. "</table>"
+      if not chrome then  -- TODO link them too.
+         html = html .. 
+            string.format("<p>Dont have chrome %s, to have chromes:</p><table>",
+                          chrome_name)
+         for k, v in pairs(register.sites) do
+            local val = v.chrome_name == k  and v.chrome_name or
+               string.format("%s != %s", k, v.chrome_name)
+            html = html .. string.format([[<tr><td><a href="/%s/">%s</a></td></tr>]],
+               val, val)
+         end
+      else
+         html = html .. string.format("<p>Dont have page %s, to have pages:</p><table>",
+                                      args.page)
+         for k in pairs(chrome.pages) do
+            html = html .. string.format([[<tr><td><a href="/%s/%s">%s</a></td></tr>]],
+               args.chrome_name, k, k)
+         end
+      end
+      return html .. "</table>"
+   end
    
    server:start(function (req, rep)
          local _,t = string.find(req.path, "/", 2, true)
@@ -16,41 +45,17 @@ return function(register)
             --uri  = req.,  --how?
             path = t2 and string.sub(req.path, t2 + 1) or "",
             whole = true,
-            --view  = "keep your hands off me",
+            --view  = "touching this limits server-ability",
             --conf
          }
-         local html = ""
          local chrome = register.sites[chrome_name]
          local page = chrome and chrome.pages[args.page]
+         local html = ""
          if page then
             html = (page.html or Suggest.html)(page, args)
          else
-            --html = string.format("<p>Try.. %d</p> %s %s %s <p>%s %s</p>", k, req.path,
-            -- t, t2, args.page, args.path)
-            html = "Dont have this..<h4>args:</h4><table>"
-            for k, v in pairs(args) do
-               html = html .. string.format("<tr><td>%s =</td><td>%s</td></tr>", k,v)
-            end
-            html = html .. "</table>"
-            if not chrome then  -- TODO link them too.
-               html = html .. 
-                  string.format("<p>Dont have chrome %s, to have chromes:</p><table>",
-                                chrome_name)
-               for k, v in pairs(register.sites) do
-                  local val = v.chrome_name == k  and v.chrome_name or
-                     string.format("%s != %s", k, v.chrome_name)
-                  html = html .. string.format("<tr><td>%s</td></tr>", val)
-               end
-            else
-               html = html .. string.format("<p>Dont have page %s, to have pages:</p><table>",
-                                            args.page)
-               for k in pairs(chrome.pages) do
-                  html = html .. string.format("<tr><td>%s</td></tr>", k)
-               end
-            end
-            html = html .. "</table>"
+            html = help_if_not_found(chrome, args)
          end
-
          rep:addHeader('Content-Type', 'text/html'):write(html)
    end)
 end
