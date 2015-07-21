aa3850f6670da90dfac2128687dbe01e5b91814b
Jasper den Ouden <o.jasper@gmail.com>
2015-07-21 21:26:38 +0200

    this works i forgot to quote strings in queries

--- sql_help/luasql_port.lua
+++ sql_help/luasql_port.lua
@@ -2,14 +2,14 @@
 
 local string_split = require "o_jasper_common.string_split"
 
-local sqlite3 = require("luasql.sqlite3").sqlite3
+local sqlite3 = require("luasql.sqlite3").sqlite3("")
 
 local Sql = {}
 
 function Sql.new(tab)
    tab = (type(tab) == "table" and tab)
       or (type(tab) == "string" and { filename = tab })
-   tab.conn = sqlite3(""):connect(tab.filename)
+   tab.conn = sqlite3:connect(tab.filename)
    return setmetatable(tab, Sql)
 end
 
@@ -52,18 +52,45 @@ local SqlCursor = {
 -- NOTE: high security risk zone.
 function Sql:exec(statement, args)  -- TODO question marks and arguments..
    args = args or {}
+   local command_str = ""
    local parts = string_split(statement, "?")
    assert( #args == #parts - 1, "not enough arguments")
    local command_str, j = "", 1
    while j < #parts do
-      command_str = command_str .. parts[j] .. self.conn:escape(tostring(args[j]))
+      local val = self.conn:escape(tostring(args[j]))
+      if type(val) == "string" then val = "'" .. val .. "'" end
+      command_str = command_str .. parts[j] .. val
       j = j + 1
    end
+   return self:list_cursor(self:_cursor(command_str))
+end
 
-  -- TODO does it :close() itself automatically? Dont see how to..
+function Sql:_cursor(command_str)
    local cursor = self.conn:execute(command_str)
-   self.conn:commit()  -- Dont really support non-committal.
-   return setmetatable({cursor = cursor, got = {}}, SqlCursor)
+   if cursor then
+      return cursor
+   else  -- Close it and try again.
+      print(command_str)
+      self.conn:close()
+      self.conn = sqlite3:connect(self.filename)
+      return self.conn:execute(command_str)
+   end
+end
+
+function Sql:list_cursor(cursor)
+   -- TODO does it :close() itself automatically? Dont see how to..
+   if cursor then
+      local ret, new = {}, {}
+      while cursor:fetch(new, "a") do
+         table.insert(ret, new)
+         new = {}
+      end
+      cursor:close()
+      return ret
+   else
+      print("no cursor")
+      return {}
+   end
 end
 
 local SqlCompiled = {
