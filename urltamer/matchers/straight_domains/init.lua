161fbe33042b2361ce1d7bf108f2f55dbf5a956f
Jasper den Ouden <o.jasper@gmail.com>
2015-08-30 15:51:30 +0200

    a bunch more patterns to allow

--- urltamer/matchers/straight_domains/init.lua
+++ urltamer/matchers/straight_domains/init.lua
@@ -7,10 +7,10 @@ Public["www.reddit.com"] = function(info, result)
    handler.everywhere(info, result,
                   {"^https?://.[.]thumbs[.]redditmedia[.]com/.+[.]jpg$",
                    "^https?://.[.]thumbs[.]redditmedia[.]com/.+[.]css",
-                   "^https://www[.]reddit.com/api/login/.+",
-                   "^https?://www[.]reddit.com/*.+",
-                   "^https?://www[.]redditstatic[.]com/*.+"})
-   if info:uri_match("^https?://pixel[.]redditmedia[.]com/pixel/of_doom[.]png.+") then
+                   "^https://www[.]reddit[.]com/api/login/.+",
+                   "^https?://www[.]reddit[.]com/*",
+                   "^https?://www[.]redditstatic[.]com/*"})
+   if info:uri_match("^https?://pixel[.]redditmedia[.]com/pixel/of_doom[.]png") then
       result.allow = false
       result.disallow = true
    else  -- And allow some api stuff.
@@ -20,7 +20,7 @@ Public["www.reddit.com"] = function(info, result)
                        "store_visits", "submit", "unhide", "unmarknsfw", "unsave", "vote",
       }
       for _, el in pairs(apilist) do
-         if string.match(info.uri, string.format("^https?://www.reddit.com/api/%s$", el)) then
+         if string.match(info.uri, string.format("^https?://www[.]reddit[.]com/api/%s$", el)) then
             result.allow = true
          end
       end
@@ -54,10 +54,14 @@ end
 
 -- Allow that other one too.
 Public["imgur.com"] = function(info, result)
-   handler.everywhere(info, result, "^http://.[.]imgur[.]com/.+")
+   handler.everywhere(info, result, "^https?://.[.]imgur[.]com/.+")
 end
 Public["i.imgur.com"] = function(info, result)
-   handler.everywhere(info, result, "^http://.[.]imgur[.]com/.+")
+   handler.everywhere(info, result, "^https?://.[.]imgur[.]com/.+")
+end
+
+Public["gfycat.com"] = function(info, result)
+   handler.everywhere(info, result, "^https?://assets[.]gfycat[.]com/")
 end
 
 --Public["duckduckgo.com"] = {
@@ -68,6 +72,14 @@ end
 Public["en.wikipedia.org"] = handler.permissive
 Public["nl.wikipedia.org"] = handler.permissive
 Public["bits.wikimedia.org"] = handler.permissive
+
+Public["simple.wikipedia.org"] = function(info, result)
+   handler.everywhere(info, result, {
+                         "^https?://meta[.]wikimedia[.]org",
+                         "^https?://en[.]wikipedia[.]org",
+                         "^https?://upload[.]wikimedia[.]org",
+   })
+end
 
 Public["okturtles.slack.com"] = handler.permissive
 
@@ -91,10 +103,11 @@ end
 
 Public["github.com"] = function(info, result)
    handler.everywhere(info, result,
-                      {"^https://github.com/.+/.+/issue_comments$",
-                       "^https://github.com/.+/.+/pullrequest_comments$",
-                       "^https://avatars[%d]+.githubusercontent.com/.+",
-                       "^https://assets[-]cdn.github.com/assets/.+"
+                      {"^https://github[.]com/.+/.+/issue_comments$",
+                       "^https://github[.]com/.+/.+/pullrequest_comments$",
+                       "^https://avatars[%d]+.githubusercontent[.]com/",
+                       "^https://assets[-]cdn[.]github[.]com/assets/",
+                       "^https://raw[.]githubusercontent[.]com/"
                       })
 end
 
@@ -109,11 +122,11 @@ Public["&about:blank$"] = handler.permissive
 
 Public["stackoverflow.com"] = function(info, result)
    handler.everywhere(info, result)
-   bland_info(info, {["^http://cdn.sstatic.net/stackoverflow/all.css.+"] = "s"})
+   bland_info(info, {["^http://cdn[.]sstatic[.]net/stackoverflow/all[.]css"] = "s"})
 end
 
 Public["fileformat.info"] = function(info, result)
-   handler.everywhere(info, result, {"^https?://emoji.fileformat.info/gemoji/.+[.]png$"})
+   handler.everywhere(info, result, {"^https?://emoji[.]fileformat[.]info/gemoji/.+[.]png$"})
 end
 
 Public["firstlook.org"] = function(info, result)
@@ -142,6 +155,53 @@ end
 
 Public["www.bloomberg.com"] = function(info, result)
    handler.everywhere(info, result, "^https?://assets.bwbx.io")
+end
+
+Public["bitbucket.org"] = function(info, result)
+   handler.everywhere(info, result, "^https?://[%w]+[.]cloudfront.net/.+[.]css$")
+end
+
+Public["twitter.com"] =  function(info, result)
+   handler.everywhere(info, result, "^https?://abs[.]twimg[.]com/a/.+")
+end
+
+Public["arstechnica.com"] = function(info, result)
+   handler.everywhere(info, result,
+                      "^https?://cdn[.]arstechnica[.]net/wp-content/themes/arstechnica/assets/css/")
+end
+
+Public["fsf.org"] = function(info, result)
+   handler.everywhere(info, result, "^https?://static[.]fsf[.]org/")
+end
+
+Public["developer.mozilla.org"] = function(info, result)
+   handler.everywhere(info, result,
+                      "^https?://developer[.]cdn[.]mozilla[.]net/media/css/.+[.].css")
+end
+
+Public["archive.is"] = function(info, result)
+   handler.everywhere(info, result,
+                      "^https?://archive.today/")
+end
+Public["archive.today"] = function(info, result)
+   handler.everywhere(info, result,
+                      "^https?://archive.is/")
+end
+
+Public["amazon.com"] = function(info, result)
+   handler.everywhere(info, result,
+                      { "^https?://z-ecx[.]images-amazon[.]com",
+                        "^https?://images-na[.]ssl-images-amazon[.]com" })
+end
+
+Public["boingboing.net"] = function(info, result)
+   handler.everywhere(info, result,
+                      "^https?://media[.]boingboing[.]net")
+end
+
+Public["www.indiegogo.com"] = function(info, result)
+   handler.everywhere(info, result,
+                      "^https?://g[%d]+[.]iggcdn[.]com/assets/.+[.]css$")
 end
 
 return Public
