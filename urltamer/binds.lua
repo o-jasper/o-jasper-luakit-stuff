4e03f4bdc08d5213e24446cc4d2650db3547e289
Jasper den Ouden <o.jasper@gmail.com>
2015-08-30 15:50:40 +0200

    Fixed urlpermissive.. Somehow it gets a single empty string, now i ignored that.

--- urltamer/binds.lua
+++ urltamer/binds.lua
@@ -16,20 +16,25 @@ if config.add_cmds == nil or config.add_cmds then
    local previously = {}
    -- Makes it permissive for a while, so stuff can load. 0 time is forever.
    local function make_permissive(w, query)
-      local args = c.string_split(query, " ")
-      local for_time, uri = 3, nil
+      local args = c.string_split(query or "", " ")
+      local for_time, uri = 20, nil
       if tonumber(args[1]) then
          for_time = tonumber(args[1])
-         domain = domain_of_uri(args[2] or w.view.uri)
+         uri = args[2]
       else
-         domain = domain_of_uri(args[1] or w.view.uri)
+         uri= args[1] and (#args[1] > 0) and args[1]
       end
+      uri = uri or w.view.uri
+      local domain = domain_of_uri(uri)
       local pat = uri_to_pattern(domain)
       previously[pat] = matchers.patterns[pat]
-      local function scratch() matchers.patterns[pat] = nil end
+      local function scratch()
+         print("back to normal:", pat)
+         matchers.patterns[pat] = previously[pat]
+      end
       matchers.patterns[pat] = permissive_for_while(for_time, scratch)
 
-      print("Made permissive:", domain, uri_to_pattern(domain))
+      print("Made permissive:", pat)
    end
    local function make_normal(w, query)
       if not query or query == "" then query = domain_of_uri(w.view.uri) end
@@ -45,4 +50,6 @@ if config.add_cmds == nil or config.add_cmds then
                   make_permissive) })
    add_cmds({ cmd("urlNormal", "urlRespond back to initial.",
                   make_normal) })
+
+   -- TODO find css and allow that from one...
 end
